743e0e2f295e842ecf1be1f45b763cdd861e0dfe
KIM-IMHONG <dongledonglerami@naver.com>
2025-04-09 01:24:01 +0900

    init commit

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+interface RouteParams {
+  coinId: string;
+}
+
+interface RouteState {
+  name: string;
+}
+function Coin() {
+  const { coinId } = useParams<RouteParams>();
+  const [loading, setLoading] = useState(true);
+  const { state } = useLocation<RouteState>();
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || loading}</Title>
+      </Header>
+      {loading ? <Loader>loading...</Loader> : null}
+    </Container>
+  );
+}
+export default Coin;
